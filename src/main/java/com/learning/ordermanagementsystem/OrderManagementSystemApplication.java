dfd1aa259f7d10379fbf94552ef8bf2dcfaf2056
erick.bruno <erick.bruno@afirma-solutions.com>
2026-03-09 17:52:57 -0600

    prueba de proteccion

--- src/main/java/com/learning/ordermanagementsystem/OrderManagementSystemApplication.java
+++ src/main/java/com/learning/ordermanagementsystem/OrderManagementSystemApplication.java
@@ -3,6 +3,9 @@ package com.learning.ordermanagementsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * OrderManagementSystemApplication
+ */
 @SpringBootApplication
 public class OrderManagementSystemApplication {
 
