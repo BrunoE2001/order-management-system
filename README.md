c9e80998ffffa94616a7ba259d41d0c92490fe70
erick.bruno <erick.bruno@afirma-solutions.com>
2026-03-09 17:16:48 -0600

    Se crea la arquitectura del proyecto

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # order-management-system
-Se pone en practica patrones de diseño y arquitectura de software con Spring Boot
+**Proyecto:**
+    Sistema de gestión de pedidos (Order Management System)
+
+**Arquitectura:**
+    Hexagonal (ports and adapters) + Clean Architecture
+
+**Patrones de diseño:**
+* Factory
+* Strategy
+* Repository
+* DTO
+* Builder
+* Facade
+* Specification
+* Dependency Injection (propio de Spring)
+
+---
+**Idea del sistema**
+Sistema simple de pedidos de una tienda.
+
+**Funcionalidades:**
+* Crear un pedido
+* Agregar productos
+* Calcular precio
+* Aplicar diferentes estrategias de descuento
+* Cambiar el estado del pedido
+
+ENDPOINTS:
+
+    Ejemplo:
+    POST /orders
+    POST /orders/{id}/items
+    POST /orders/{id}/checkout
+    POST /orders/{id}/
--- src/test/java/com/learning/ordermanagementsystem/OrderManagementSystemApplicationTests.java
+++ src/test/java/com/learning/ordermanagementsystem/OrderManagementSystemApplicationTests.java
@@ -1,8 +1,15 @@
 package com.learning.ordermanagementsystem;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ExtendWith(OutputCaptureExtension.class)
 @SpringBootTest
 class OrderManagementSystemApplicationTests {
 
@@ -10,4 +17,15 @@ class OrderManagementSystemApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void testMain(CapturedOutput output) {
+        OrderManagementSystemApplication.main(new String[]{
+                "--spring.main.web-application-type=none", // No levanta Tomcat
+                "--spring.main.lazy-initialization=true",  // No crea beans innecesarios
+                "--spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration" // Evita conectar a DB
+        });
+
+        // Verificamos que en la consola aparezca el mensaje típico de inicio de Spring Boot
+        assertThat(output.getOut()).contains("Started OrderManagementSystemApplication");
+    }
 }
